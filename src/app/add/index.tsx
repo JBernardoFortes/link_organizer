e3b0ace24494c03a4ab13acde3ec815945b5877d
Bernardo <joseberk2@gmail.com>
2026-07-12 18:55:08 -0300

    feat: create add screen

---
+++ src/app/add/index.tsx
@@ -0,0 +1,24 @@
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import { categories } from "@/utils/categories";
+
+export default function Add() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons
+            name="arrow-back"
+            size={32}
+            color={colors.gray[200]}
+          ></MaterialIcons>
+        </TouchableOpacity>
+        <Text style={styles.title}> Novo </Text>
+      </View>
+      <Text style={styles.label}>Selecione uma categoria</Text>
+    </View>
+  );
+}
