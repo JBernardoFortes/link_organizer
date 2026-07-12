93ede4b8d0262506760060b3b9cff958f766ca30
Bernardo <joseberk2@gmail.com>
2026-07-12 11:50:03 -0300

    feat: add interface for props and make the component more dynamic

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,13 +1,18 @@
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Pressable, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
 import { styles } from "./styles";
 
-export function Category() {
+interface Props extends PressableProps {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  name: string;
+}
+
+export function Category({ icon, name, ...rest }: Props) {
   return (
-    <Pressable style={styles.container}>
-      <MaterialIcons name="code" size={16} color={colors.gray[400]} />
-      <Text style={styles.name}>Projetos</Text>
+    <Pressable style={styles.container} {...rest}>
+      <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
+      <Text style={styles.name}>{name}</Text>
     </Pressable>
   );
 }
