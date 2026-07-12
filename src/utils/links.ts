5d35b29fd22fd1c8d7bb7b8d00ed71cac0c74735
Bernardo <joseberk2@gmail.com>
2026-07-12 13:35:07 -0300

    feat: add mock links file

---
+++ src/utils/links.ts
@@ -0,0 +1,23 @@
+interface Link {
+  id: string;
+  name: string;
+  url: string;
+}
+
+export const links: Link[] = [
+  {
+    id: "1",
+    name: "Google",
+    url: "https://google.com.br",
+  },
+  {
+    id: "2",
+    name: "Github",
+    url: "https://github.com",
+  },
+  {
+    id: "3",
+    name: "Facebook",
+    url: "https://facebook.com",
+  },
+];
