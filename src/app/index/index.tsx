d0b449d31c2924e2867a085ff74c2a7e60f541ca
Bernardo <joseberk2@gmail.com>
2026-07-12 18:25:16 -0300

    feat: create and add modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,17 @@
 // Ponto de entrada da aplicacao
 import { CategoryList } from "@/components/CategoryList";
 import { Link } from "@/components/Link";
+import { Option } from "@/components/Option";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 
@@ -41,6 +49,31 @@ export default function Index() {
           contentContainerStyle={styles.linksContent}
           showsVerticalScrollIndicator={false}
         />
+
+        <Modal transparent visible={true}>
+          <View style={styles.modal}>
+            <View style={styles.modalContent}>
+              <View style={styles.modalHeader}>
+                <Text style={styles.modalCategory}>Curso</Text>
+                <TouchableOpacity>
+                  <MaterialIcons
+                    name="close"
+                    size={20}
+                    color={colors.gray[400]}
+                  ></MaterialIcons>
+                </TouchableOpacity>
+              </View>
+
+              <Text style={styles.modalLinkName}> Google </Text>
+              <Text style={styles.modalUrl}> https://google.com.br</Text>
+
+              <View style={styles.modalFooter}>
+                <Option name="Excluir" icon="delete" variant="secondary" />
+                <Option name="Abrir" icon="language" />
+              </View>
+            </View>
+          </View>
+        </Modal>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -30,4 +30,47 @@ export const styles = StyleSheet.create({
     gap: 20,
     padding: 24,
   },
+  modalContent: {
+    backgroundColor: colors.gray[900],
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+    paddingBottom: 42,
+    padding: 24,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: colors.gray["transparent"],
+  },
+  modalHeader: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  modalFooter: {
+    flexDirection: "row",
+    marginTop: 32,
+    width: "100%",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+    paddingVertical: 14,
+  },
+  modalCategory: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 500,
+    color: colors.gray[400],
+  },
+  modalUrl: {
+    fontSize: 24,
+    color: colors.gray[400],
+  },
+  modalLinkName: {
+    fontSize: 18,
+    fontWeight: 600,
+    color: colors.gray[200],
+  },
 });
