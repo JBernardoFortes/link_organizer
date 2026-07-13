6501a9f499ea2ba81417820d201845ba1f97fa0c
Bernardo <joseberk2@gmail.com>
2026-07-13 18:22:31 -0300

    feat: add get and save functions to use async storage

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINK_STORAGE_KEY = "links-storage";
+
+interface LinkStorage {
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+}
+
+const getLinks = async (): Promise<LinkStorage[]> => {
+  const storage = await AsyncStorage.getItem(LINK_STORAGE_KEY);
+  const response = storage ? JSON.parse(storage) : [];
+
+  return response;
+};
+
+const saveLink = async (newLink: LinkStorage) => {
+  try {
+    const storage = await getLinks();
+    const updatedStorage = [...storage, newLink];
+
+    await AsyncStorage.setItem(
+      LINK_STORAGE_KEY,
+      JSON.stringify(updatedStorage),
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const linkStorage = {
+  getLinks,
+  saveLink,
+};
