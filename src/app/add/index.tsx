d2071a29b9681401fae2211076061e4dbe344579
Bernardo <joseberk2@gmail.com>
2026-07-13 17:42:59 -0300

    feat: add Inputs to create new Link and check the data passed

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -4,10 +4,32 @@ import { Input } from "@/components/Input";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export default function Add() {
+  const [name, setName] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+
+  const handleChangeName = (text: string) => setName(text);
+  const handleChangeUrl = (text: string) => setUrl(text);
+
+  const handleAdd = () => {
+    if (!category) {
+      return Alert.alert("Categoria", "Selecione uma categoria");
+    }
+    if (!url.trim()) {
+      return Alert.alert("URL", "Informe uma URL");
+    }
+    if (!name.trim()) {
+      return Alert.alert("Name", "Informe um nome");
+    }
+    console.log(name);
+    console.log(url);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -21,12 +43,15 @@ export default function Add() {
         <Text style={styles.title}> Novo </Text>
       </View>
       <Text style={styles.label}>Selecione uma categoria</Text>
-      <CategoryList></CategoryList>
+      <CategoryList
+        onChangeCategory={setCategory}
+        selected={category}
+      ></CategoryList>
 
       <View style={styles.form}>
-        <Input placeholder="name" />
-        <Input placeholder="url" />
-        <Button title="Adicionar"></Button>
+        <Input placeholder="Name" onChangeText={handleChangeName} />
+        <Input placeholder="URL" onChangeText={handleChangeUrl} />
+        <Button title="Adicionar" onPress={handleAdd}></Button>
       </View>
     </View>
   );
